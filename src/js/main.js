962f5932359cfa135d8cd22ddb2bed0789ef0ab1
Viacheslav <zvv56739@gmail.com>
2021-07-15 23:22:21 +0300

    add arrow elem for accordion and change jq scripts

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
     $('.accordion__faq').click(function(){
+        $(this).toggleClass('accordion__faq--active');
         $(this).next('.accordion__answer').slideToggle(200);
-    })
+        $(this).nextAll('.accordion__arrow').toggleClass('accordion__arrow--active');
+    });
     $('.header__btn').click(function() {
         $([document.documentElement, document.body]).animate({
             scrollTop: $('.info').offset().top
