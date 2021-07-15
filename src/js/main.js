a3d4f21fceb47e493bde53fec9a8486eaa0a9edd
Viacheslav <zvv56739@gmail.com>
2021-07-15 19:55:22 +0300

    scroll to info section, fix header bg

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,12 @@ $(document).ready(function(){
     $('.accordion__faq').click(function(){
         $(this).next('.accordion__answer').slideToggle(200);
     })
-})
+    $('.header__btn').click(function() {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $('.info').offset().top
+        }, 1000);
+    });
+});
 
 
 
