3329cdf5e8c074171151bb0441c9f52b512626a2
Viacheslav <zvv56739@gmail.com>
2021-07-15 19:33:58 +0300

    init

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+var 	gulp           = require('gulp'),
+		sass           = require('gulp-sass'),
+		browserSync    = require('browser-sync'),
+		concat         = require('gulp-concat'),
+		uglify         = require('gulp-uglify'),
+		cleanCSS       = require('gulp-clean-css'),
+		rename         = require('gulp-rename'),
+		autoprefixer   = require('gulp-autoprefixer'),
+		notify         = require("gulp-notify");
+
+// Сервер и автообновление страницы Browsersync
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false,
+		// tunnel: true,
+	});
+});
+
+// Минификация пользовательских скриптов проекта и JS библиотек в один файл
+gulp.task('js', function() {
+	return gulp.src([
+		'./src/js/main.js', // Всегда в конце
+		])
+	.pipe(concat('scripts.min.js'))
+	.pipe(uglify()) // Минимизировать весь js (на выбор)
+	.pipe(gulp.dest('app/js'))
+	.pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('sass', function() {
+	return gulp.src('./src/scss/main.scss')
+	.pipe(sass({outputStyle: 'compressed'}).on("error", notify.onError()))
+	.pipe(rename({suffix: '.min', prefix : ''}))
+	.pipe(autoprefixer(['last 15 versions']))
+	.pipe(cleanCSS()) // Опционально, закомментировать при отладке
+	.pipe(gulp.dest('./app/css'))
+	.pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('code', function() {
+	return gulp.src('app/**/*.html')
+	.pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('watch', function() {
+	gulp.watch('./src/scss/**/*.scss', gulp.parallel('sass'));
+	gulp.watch(['./src/js/main.js'], gulp.parallel('js'));
+	gulp.watch('app/*.html', gulp.parallel('code'));
+});
+
+gulp.task('default', gulp.parallel('sass', 'js', 'browser-sync', 'watch'));
---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.accordion__faq').click(function(){
+        $(this).next('.accordion__answer').slideToggle(200);
+    })
+})
+
+
+
