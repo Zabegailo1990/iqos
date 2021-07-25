ec3a029fecda28b7f3f35003c9f5144a263b7f86
Viacheslav <zvv56739@gmail.com>
2021-07-26 00:12:43 +0300

    fix accordion

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,11 @@
 $(document).ready(function(){
     $('.accordion__faq').click(function(){
-        $(this).toggleClass('accordion__faq--active');
         $(this).next('.accordion__answer').slideToggle(200);
-        $(this).nextAll('.accordion__arrow').toggleClass('accordion__arrow--active');
+        $(this).toggleClass('accordion__faq--active');
     });
-    $('.header__btn').click(function() {
+    $('.header__btn').click(function(){
         $([document.documentElement, document.body]).animate({
             scrollTop: $('.info').offset().top
         }, 1000);
     });
 });
-
-
-
